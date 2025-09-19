185588ab5d898f03d9af45120017cf78a4547f89
insidevArch <insi.dev12@gmail.com>
2025-09-18 21:57:14 -0300

    feat/fix: register componenet is added

---
+++ front/src/features/Register.tsx
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import {useAuth} from './auth/useAuth.ts'
+import { observer } from "mobx-react-lite";
+import { userApp } from "./user/userType.ts";
+//
+// interface LoginProps {
+//   onLogin: (username: string, password: string) => void;
+// }
+const Login = observer(() => {
+  // usamos el auth
+  const { login } = useAuth();
+
+  // estado local
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [rol, setRol] = useState("");
+
+  // function cuando el usuario clickea el boton
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const user: userApp = {username, password}
+      await login(user);
+
+    } catch (err) {
+      alert("error en el login: " + err)
+
+    }
+    //onLogin(username, password);
+    
+    // Limpiar inputs si quieres
+    setUsername("");
+    setPassword("");
+    setRol("");
+  };
+
+  return (
+    <div style={{ maxWidth: "400px", margin: "0 auto", padding: "2rem", border: "1px solid #ccc", borderRadius: "8px" }}>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            style={{ width: "100%", padding: "0.5rem", marginTop: "0.5rem" }}
+            required
+          />
+        </div>
+
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+
+            style={{ width: "100%", padding: "0.5rem", marginTop: "0.5rem" }}
+            required
+          />
+        </div>
+
+        <div style={{ marginBottom: "1rem" }}>
+          <label htmlFor="role">Rol:</label>
+          <select
+            id="role"
+            value={rol} // suponiendo que tenés un state: const [role, setRole] = useState("")
+            onChange={(e) => setRol(e.target.value)}
+            style={{ width: "100%", padding: "0.5rem", marginTop: "0.5rem" }}
+            required
+          >
+            <option value="">Seleccione un rol</option>
+            <option value="admin">Administrador</option>
+            <option value="user">Usuario</option>
+            <option value="guest">Invitado</option>
+          </select>
+        </div>
+        <button type="submit" style={{ padding: "0.5rem 1rem" }}>Login</button>
+      </form>
+    </div>
+  );
+
+
+  })
+export default Login;
+
