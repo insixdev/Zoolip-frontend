56d58149e841bde60546729a09dab187e135b79e
insidevArch <insi.dev12@gmail.com>
2025-10-03 12:46:59 -0300

    undefined exp

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,18 +2,23 @@
 import { JSX } from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
+import { Navbar } from './components/layout/navbar';
 function App(): JSX.Element {
 
   // const [count, setCount] = useState(0)
   return (
-    <div className="app-container">
+
+    <>
+      <Navbar/>
+      {/*   <nav className="nav-links"> */}
+      {/*     <Link to="/login">Login</Link> */}
+      {/*     <Link to="/register">Register</Link> */}
+      {/*     <Link to="/profile">Profile</Link> */}
+      {/*   </nav> */}
+
       <h1>Mi App Croteria</h1>
-      <nav className="nav-links">
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/profile">Profile</Link>
-      </nav>
-    </div>
-  )
+
+    </>
+  );
 }
 export default App
